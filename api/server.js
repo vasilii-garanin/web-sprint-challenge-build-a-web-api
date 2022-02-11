46658b67d8f135adb316e10128384e75ba8d5c3c
vasilii-garanin <vasilii_garanin@laptevfamily.com>
2022-02-11 13:15:54 -0600

    WIP

--- api/server.js
+++ api/server.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const server = express();
 
+const cors = require('cors');
+
+const projectRouter = require('./projects/projects-router');
+
+const actionRouter = require('./actions/actions-router');
+
+server.use(cors());
+
+server.use(express.json());
+
+// server.use('/api/projects', projectRouter);
+
+// server.use('/api/actions', actionRouter);
+
+server.get('/', (req, res) =>
+{
+    res.send(`
+      <h2>Project&Action Page</h2>
+      <p>Welcome to the Project&Action API</p>
+    `);
+});
+
+server.use('*', (req, res) => 
+{
+    res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
+});
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
